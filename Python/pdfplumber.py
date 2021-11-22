24f059bba9ced77feb2d68ea54c08a26242d2a00
Kumar <kshivam654@gmail.com>
2021-11-22 20:05:48 +0530

    tika fixed.

---
+++ Python/pdfplumber.py
@@ -0,0 +1,4 @@
+import pdfplumber
+with pdfplumber.open("example.pdf") as pdf:
+    first_page = pdf.pages[0]
+    print(first_page.extract_text())
--- Python/pdfreadertika.py
+++ Python/pdfreadertika.py
@@ -1,8 +1,10 @@
 from tika import parser
 
-raw = parser.from_file("example.pdf")
+raw = parser.from_file("a.pdf")
 raw = str(raw['content'])
 
 safe_text = raw.encode('utf-8', errors='ignore')
-safe_text = str(safe_text).replace("\n", "").replace("\\", "")
+safe_text = str(safe_text)
+safe_text = safe_text.replace(r'\n', '')
+safe_text = safe_text.split('docx')[1]
 print( safe_text )
