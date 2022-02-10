ae4a97004027f533023812830aa0c16670126889
shivam <kshivam654@gmail.com>
2022-02-10 11:56:52 +0530

    leetcode and algoexpert.

--- AlgoExpert/single_cycle_check.py
+++ AlgoExpert/single_cycle_check.py
@@ -1,14 +1,24 @@
-# O(N) time | O(1) space
-def hasSingleCycle(array):
-    numElementsVisited = 0
-    currentIdx = 0
-    while numElementsVisited < len(array):
-        if numElementsVisited > 0 and currentIdx == 0:
+# https://www.algoexpert.io/questions/Single%20Cycle%20Check
+
+
+# O(n) time | O(1) space - where n is the length of the input array
+def has_single_cycle(array):
+    number_of_elements_visited = 0
+    current_index = 0
+    while number_of_elements_visited < len(array):
+        if number_of_elements_visited > 0 and current_index == 0:
             return False
-        numElementsVisited += 1
-        currentIdx = getNextIdx(currentIdx, array)
-    return currentIdx == 0
-def getNextIdx(currentIdx, array):
-    jump = array[currentIdx]
-    nextIdx = (currentIdx + jump) % len(array)
-    return nextIdx if nextIdx > 0 else nextIdx + len(array)
+        number_of_elements_visited += 1
+        current_index = get_next_index(current_index, array)
+    return current_index == 0
+
+
+def get_next_index(current_index, array):
+    jump = array[current_index]
+    next_index = (current_index + jump) % len(array)
+    return next_index if next_index >= 0 else next_index + len(array)
+
+
+input_array = [2, -1, 1, 2, 2]
+print("Is Single Cycle: ", has_single_cycle(input_array))
+
